65c837cbe6e4bddbad8b8fc05dcb3aa8585c7510
Alam García Roldán <40043129+alamgarcia@users.noreply.github.com>
2019-05-07 19:33:33 -0500

    Cambios UI

--- app/src/main/java/com/garcia76/clientsdk/CallActivity.kt
+++ app/src/main/java/com/garcia76/clientsdk/CallActivity.kt
@@ -47,7 +47,6 @@ class CallActivity : AppCompatActivity() {
         }
         false
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
